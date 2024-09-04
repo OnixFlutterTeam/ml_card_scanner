652a312dbea68bb6e4f686084a84f6ece5dd82bb
Ivan <vanek975@gmail.com>
2024-09-04 13:49:02 +0300

    feat(Typedefs): Added typedef instead of void Function

--- lib/src/model/typedefs.dart
+++ lib/src/model/typedefs.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/widgets.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:ml_card_scanner/src/widget/camera_preview_wrapper.dart';
 
 typedef CameraPreviewBuilder = Widget Function(
-    BuildContext context,
-    CameraPreviewWrapper preview,
-    Size? previewSize,
-    );
+  BuildContext context,
+  CameraPreviewWrapper preview,
+  Size? previewSize,
+);
 
 typedef OverlayTextBuilder = Widget Function(BuildContext context);
 
 typedef OverlayBuilder = Widget Function(BuildContext context);
+
+typedef InputImageCallback = void Function(InputImage inputImage);
